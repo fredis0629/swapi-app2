fbd2677f4d30e02b68170e91fb52faec95bb464e
fredis0629 <fredis0629@gmail.com>
2019-08-16 16:05:11 +0300

    Custom Radio Buttons

--- src/header.js
+++ src/header.js
@@ -3,20 +3,36 @@ import styled from "styled-components";
 
 const Hed = styled.header`
   display: flex;
-  width: "100wv";
+  width: 100vw;
   justify-content: center;
   background-color: #000;
-  a {
+  height: 50px;
+  label {
     cursor: pointer;
-    height: 50px;
+    height: 100%;
     text-align: center;
     display: flex;
     align-items: center;
     background-color: #000;
     color: #fff;
-    padding: 0 10px;
+    input {
+      position: absolute;
+      opacity: 0;
+      cursor: pointer;
+    }
+    input:checked ~ .checkmark {
+      background-color: #2196f3;
+    }
+    .checkmark {
+      height: 100%;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 0 10px;
+    }
   }
-  a:hover {
+  label:hover {
     background-color: #f0f0f0;
     color: #000;
   }
@@ -27,9 +43,10 @@ class Header extends React.Component {
     return (
       <Hed>
         {Object.keys(this.props.objOfHeaderField).map(val => (
-          <a key={val} onClick={() => this.props.getFields("objOfContentList", this.props.objOfHeaderField[val])}>
-            {val}
-          </a>
+          <label key={val} onClick={e => this.props.getFields("objOfContentList", this.props.objOfHeaderField[val])}>
+            <input type="radio" name="header" />
+            <span className="checkmark">{val}</span>
+          </label>
         ))}
       </Hed>
     );
